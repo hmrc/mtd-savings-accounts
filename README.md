7921e0e00b21183fd493b2c61010b612b3484a5c
Daniel Andrews <38686400+daniel-andrews1994@users.noreply.github.com>
2019-02-12 11:10:45 +0000

    Upgraded sbt version and added tests to increase coverage (#1)

--- README.md
+++ README.md
@@ -1,8 +1,10 @@
-
 # mtd-savings-accounts
 
-This is a placeholder README.md for a new repository
+[![Apache-2.0 license](http://img.shields.io/badge/license-Apache-brightgreen.svg)](http://www.apache.org/licenses/LICENSE-2.0.html)
+[![Download](https://api.bintray.com/packages/hmrc/releases/mtd-savings-accounts/images/download.svg) ](https://bintray.com/hmrc/releases/mtd-savings-accounts/_latestVersion)
+
+This service manages the mtd-savings-accounts.
 
 ### License
 
-This code is open source software licensed under the [Apache 2.0 License]("http://www.apache.org/licenses/LICENSE-2.0.html").
+This code is open source software licensed under the [Apache 2.0 License]("http://www.apache.org/licenses/LICENSE-2.0.html")
---
+++ app/v2/config/AppConfig.scala
@@ -0,0 +1,42 @@
+/*
+ * Copyright 2019 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package v2.config
+
+import javax.inject.{Inject, Singleton}
+import uk.gov.hmrc.play.bootstrap.config.ServicesConfig
+
+
+trait AppConfig {
+  def desBaseUrl: String
+
+  def mtdIdBaseUrl: String
+
+  def desEnv: String
+
+  def desToken: String
+}
+
+@Singleton
+class AppConfigImpl @Inject()(config: ServicesConfig) extends AppConfig {
+
+
+  val mtdIdBaseUrl: String = config.baseUrl("mtd-id-lookup")
+  val desBaseUrl: String = config.baseUrl("des")
+  val desEnv: String = config.getString("microservice.services.des.env")
+  val desToken: String = config.getString("microservice.services.des.token")
+
+}
