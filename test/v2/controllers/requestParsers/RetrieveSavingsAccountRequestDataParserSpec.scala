230304d756fb71140ffeff8572a9d2698212be49
James Whiteley <31282758+james-work-account@users.noreply.github.com>
2019-02-20 15:08:11 +0000

    [MTDSA-2989] added request parser and validator for retrieve all

---
+++ test/v2/controllers/requestParsers/RetrieveSavingsAccountRequestDataParserSpec.scala
@@ -0,0 +1,86 @@
+/*
+ * Copyright 2019 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package v2.controllers.requestParsers
+
+import support.UnitSpec
+import uk.gov.hmrc.domain.Nino
+import v2.mocks.validators.MockRetrieveSavingsAccountValidator
+import v2.models.errors.{AccountNameDuplicateError, BadRequestError, ErrorWrapper, NinoFormatError}
+import v2.models.requestData.{RetrieveSavingsAccountRawData, RetrieveSavingsAccountRequest}
+
+class RetrieveSavingsAccountRequestDataParserSpec extends UnitSpec {
+
+  val validNino = "AA123456A"
+  val correlationId = "X-123"
+
+  trait Test extends MockRetrieveSavingsAccountValidator {
+    lazy val parser = new RetrieveSavingsAccountRequestDataParser(mockValidator)
+  }
+
+
+  "parse" should {
+
+    "return a retrieve savings account request object" when {
+      "valid request data is supplied" in new Test {
+
+        val retrieveSavingsAccountRawData =
+          RetrieveSavingsAccountRawData(validNino)
+
+        val retrieveSavingsAccountRequest =
+          RetrieveSavingsAccountRequest(Nino(validNino))
+
+        MockedCreateSavingsAccountValidator.validate(retrieveSavingsAccountRawData)
+          .returns(List())
+
+        parser.parseRequest(retrieveSavingsAccountRawData) shouldBe Right(retrieveSavingsAccountRequest)
+      }
+    }
+
+    "return an ErrorWrapper" when {
+
+      val invalidNino = "AA112A"
+
+      "a single validation error occurs" in new Test {
+        val retrieveSavingsAccountRawData =
+          RetrieveSavingsAccountRawData(invalidNino)
+
+        val expectedResponse =
+          ErrorWrapper(None, NinoFormatError, None)
+
+        MockedCreateSavingsAccountValidator.validate(retrieveSavingsAccountRawData)
+          .returns(List(NinoFormatError))
+
+        parser.parseRequest(retrieveSavingsAccountRawData) shouldBe Left(expectedResponse)
+      }
+
+      "multiple validation errors occur" in new Test {
+        val retrieveSavingsAccountRawData =
+          RetrieveSavingsAccountRawData(validNino)
+
+        val multipleErrorWrapper =
+          ErrorWrapper(None, BadRequestError, Some(Seq(NinoFormatError, AccountNameDuplicateError)))
+
+        MockedCreateSavingsAccountValidator.validate(retrieveSavingsAccountRawData)
+          .returns(List(NinoFormatError, AccountNameDuplicateError))
+
+
+        parser.parseRequest(retrieveSavingsAccountRawData) shouldBe Left(multipleErrorWrapper)
+      }
+    }
+
+  }
+}
---
+++ test/v2/controllers/requestParsers/validators/RetrieveSavingsAccountValidatorSpec.scala
@@ -0,0 +1,57 @@
+/*
+ * Copyright 2019 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package v2.controllers.requestParsers.validators
+
+import support.UnitSpec
+import v2.models.errors._
+import v2.models.requestData.RetrieveSavingsAccountRawData
+
+class RetrieveSavingsAccountValidatorSpec extends UnitSpec {
+
+  val validNino = "AA123456A"
+
+  private trait Test {
+    val validator = new RetrieveSavingsAccountValidator()
+  }
+
+  "running a validation" when {
+
+    "the uri is valid and the JSON payload is Valid with all fields" should {
+      "return no errors" in new Test {
+        val inputData =RetrieveSavingsAccountRawData(validNino)
+
+        val result: Seq[Error] = validator.validate(inputData)
+
+        result shouldBe List()
+
+      }
+    }
+
+    "an invalid nino is supplied" should {
+      "return nino format error" in {
+        val nino = "AA1456A"
+        val expectedData = List(NinoFormatError)
+        val requestRawData = RetrieveSavingsAccountRawData(nino)
+
+        val result = new RetrieveSavingsAccountValidator().validate(requestRawData)
+
+        result shouldBe expectedData
+      }
+    }
+  }
+
+}
