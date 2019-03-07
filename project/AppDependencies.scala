cb44d055222bda7917dca713e71cb9cb093060c5
James Whiteley <31282758+james-work-account@users.noreply.github.com>
2019-03-07 14:27:22 +0000

    [MTDSA-2359] Create Explicit Audit at Add a Savings Account (#24)
    * [MTDSA-2359] created audit models
    
    * [MTDSA-2359] upgraded libraries in preparation of audit service layer
    
    * [MTDSA-2359] added audit service
    
    * [MTDSA-2359] added success and error audit scenarios

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -22,9 +22,9 @@ object AppDependencies {
 
   val compile = Seq(
     ws,
-    "uk.gov.hmrc" %% "bootstrap-play-26" % "0.34.0",
+    "uk.gov.hmrc" %% "bootstrap-play-26" % "0.36.0",
     "uk.gov.hmrc" %% "domain" % "5.3.0",
-    "org.typelevel" %% "cats-core" % "1.1.0"
+    "org.typelevel" %% "cats-core" % "1.6.0"
   )
 
   def test(scope: String = "test, it"): Seq[sbt.ModuleID] = Seq(
@@ -36,7 +36,7 @@ object AppDependencies {
 
     "org.scalatestplus.play" %% "scalatestplus-play" % "3.1.2" % scope,
 
-    "com.github.tomakehurst" % "wiremock" % "2.20.0" % scope  )
+    "com.github.tomakehurst" % "wiremock" % "2.21.0" % scope  )
 
 
   // Fixes a transitive dependency clash between wiremock and scalatestplus-play
