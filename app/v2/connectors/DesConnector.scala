4404ccf4e7e976192c9afd9ec46d984a0fdeebce
Taiwo-AB <63714619+Taiwo-AB@users.noreply.github.com>
2020-12-17 13:00:15 +0000

    MTDSA-7872 Update dependencies (#61)
    * MTDSA-7872 Update dependencies
    
    * MTDSA-7872 Update dependencies

--- app/v2/connectors/DesConnector.scala
+++ app/v2/connectors/DesConnector.scala
@@ -20,7 +20,7 @@ import javax.inject.{Inject, Singleton}
 import play.api.Logger
 import uk.gov.hmrc.http.HeaderCarrier
 import uk.gov.hmrc.http.logging.Authorization
-import uk.gov.hmrc.play.bootstrap.http.HttpClient
+import uk.gov.hmrc.http.HttpClient
 import v2.config.AppConfig
 import v2.httpparsers.StandardDesHttpParser
 import v2.models.des.{DesAmendSavingsAccountAnnualSummaryResponse, DesRetrieveSavingsAccountAnnualIncomeResponse, DesSavingsInterestAnnualIncome}
